fccac1b016e461c30de9433992cf0bf3eee8a564
Roger_13222099 <rogersupriyanto@gmail.com>
2026-01-07 23:06:32 +0700

    commit

--- src/rtl/shared/activation_unit/code/pwl_activation.v
+++ src/rtl/shared/activation_unit/code/pwl_activation.v
@@ -2,18 +2,9 @@ module pwl_activation(
     input  wire signed [15:0] d_in,   // Input Q4.12 Signed
     output reg  signed [15:0] d_out   // Output Q4.12 Signed (Range: -1.0 s.d 1.0)
 );
-
-    // --- Definisi Konstanta Q4.12 ---
-    // Format Q4.12: 1.0 = 1 << 12 = 4096
-    
-    // NILAI SATURASI (OUTPUT)
     localparam signed [15:0] VAL_ONE       =  16'sd4096; //  1.0
     localparam signed [15:0] VAL_MINUS_ONE = -16'sd4096; // -1.0
 
-    // BATAS INPUT (THRESHOLD)
-    // Rumus: y = 2x. 
-    // Saturasi atas terjadi saat 2x = 1.0 -> x = 0.5
-    // 0.5 dalam Q4.12 = 4096 / 2 = 2048
     
     localparam signed [15:0] UPPER_THRESH =  16'sd2048; //  0.5
     localparam signed [15:0] LOWER_THRESH = -16'sd2048; // -0.5
